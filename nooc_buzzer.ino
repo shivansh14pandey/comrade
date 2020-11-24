ef0b78d229879c3fd0fdff2a3d4a9bbe38c6632b
Shivansh Pandey <72841823+shivansh14pandey@users.noreply.github.com>
2020-11-24 11:26:18 +0530

    bonus task 2 program

---
+++ nooc_buzzer.ino
@@ -0,0 +1,39 @@
+int i,j;
+float myV;
+int readval=A2;
+void setup() {
+  Serial.begin(9600);
+  pinMode(A2,INPUT);
+  for (i=6; i<11 ; i++)
+  {
+    pinMode(i,OUTPUT);
+  }
+
+}
+
+void loop() {
+ j=analogRead(readval);
+ myV=(7.0/1023.0)*j;
+ if(myV>=5){
+ digitalWrite(12,HIGH);
+ delay(200);
+ digitalWrite(12,LOW);
+ delay(200);
+  
+ }
+ else{
+ if(myV>=0){
+  
+    for(i=5;i<=myV+5;i++){
+      digitalWrite(i,HIGH);
+      delay(100);
+    }
+    for(i=6+myV;i<=10;i++){
+      digitalWrite(i,LOW);
+      delay(100);
+    }
+ }
+    
+  }
+ }
+   
